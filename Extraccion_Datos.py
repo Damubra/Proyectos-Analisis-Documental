efa012b90f5e25d1132f88b99918db3cfb050dc5
Damubra <damubra18@gmail.com>
2025-03-19 21:25:01 -0600

    Se corrigió la lectura de la ruta de acceso con la función strip() y remplazando \ por /

--- Extraccion_Datos.py
+++ Extraccion_Datos.py
@@ -1,19 +1,19 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe' #CON ESTA LINEA NOS EVITAMOS LO DEL PATH
 
 #--------------------PARTE 1. PROCESAMIENTO DE LA IMAGEN A TRABAJAR----------------------------------------------------------------
 
-documento = input(str("Introduce la ruta de la imagen: ")) #INGRESAR LA RUTA DE LA IMAGEN (TIP: IMAGEN > DETALLES)
+documento = input("Introduce la ruta de la imagen: ").strip("'").strip('"') #INGRESAR LA RUTA DE LA IMAGEN (TIP: IMAGEN > DETALLES)
+documento = documento.replace("\\", "/") #CON ESTO CORREGIMOS EL ERROR DE QUE NO SE PUEDEN LEER LAS RUTAS QUE TIENEN '\'
 imagen_0 = Image.open(documento) #DEFINIMOS LA VARIABLE IMAGEN
 
 ancho, alto = imagen_0.size #DEFINIMOS EL TAMAÑO DE LA IMAGEN
-imagen = imagen_0.crop((0, 0, ancho*0.70, alto))  #RECORTAMOS HORIZONTALMENTE LA IMAGEN A LA MITAD
+imagen = imagen_0.crop((0, 0, ancho*0.70, alto))  #RECORTAMOS HORIZONTALMENTE LA IMAGEN AL 70%
 
 texto = pytesseract.image_to_string(imagen, lang="spa") # ESTO ES PARA EXTRAER EL TEXTO ("SPA" ES ESPAÑOL)
 lineas = [linea.strip() for linea in texto.split("\n") if linea.strip()] #ORGANIZAMOS EL TEXTO POR LINEAS
-
 
 
 #--------------------PARTE 2. BUSQUEDA DE LOS DATOS DE INTERES---------------------------------------------------------
@@ -25,9 +25,9 @@ for i, linea in enumerate(lineas):
         
 for i, linea in enumerate(lineas):  # IDENTIFICA EL TEXTO DE LAS PRIMERAS TRES LINEAS DESPUES DE LA PALABRA DOMICILIO
     if "DOMICILIO" in linea:
-        Domicilio = " ".join(lineas[i+1:i+4])
+        domicilio = " ".join(lineas[i+1:i+4])
         break
 #----------------PARTE 3. RESULTADOS---------------------------------------------------------------------
 
 print("Nombre:", nombre_completo)
-print("Domicilio:", Domicilio)
+print("Domicilio:", domicilio)
