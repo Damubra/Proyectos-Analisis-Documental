58f98e96c82507c6d72e645497ba592f3adf3e6c
Damubra <damubra18@gmail.com>
2025-03-22 17:13:34 -0600

    Se agrego un convertidor PDF a JPG para subir la ruta de documentos PDF. Se añadió un mensaje de salida en caso de que no se pueda extraer el texto

--- Extraccion_Datos.py
+++ Extraccion_Datos.py
@@ -2,47 +2,36 @@ import pytesseract
 from PIL import Image
 from pdf2image import convert_from_path
 
-# Configura la ruta de Tesseract-OCR
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe' #CON ESTA LINEA NOS EVITAMOS LO DEL PATH
 
-#--------------------PARTE 1. PROCESAMIENTO DEL PDF A IMAGEN----------------------------------------------------------------
+#--------------------PARTE 1. PROCESAMIENTO DEL DOCUMENTO A TRABAJAR----------------------------------------------------------------
 
-# Solicita la ruta del archivo PDF
-documento = input("Introduce la ruta del archivo: ").strip("'").strip('"')
-documento = documento.replace("\\", "/")  # Corrige las rutas con '\'
+documento = input("Introduce la ruta de la imagen: ").strip("'").strip('"') #INGRESAR LA RUTA DE LA IMAGEN (TIP: IMAGEN > DETALLES)
+documento = documento.replace("\\", "/")#CON ESTO CORREGIMOS EL ERROR DE QUE NO SE PUEDEN LEER LAS RUTAS QUE TIENEN '\'
+jpg = convert_from_path(documento) #CONVERTIMOS EL DOCUMENTO DE PDF A JPG
+imagen_0 = jpg[0] #OCUPAMOS SOLO LA PRIMER PAGINA
 
-# Convierte el PDF a una lista de imágenes
-jpg = convert_from_path(documento)
+ancho, alto = imagen_0.size #DEFINIMOS EL TAMAÑO DE LA IMAGEN
+imagen = imagen_0.crop((0, 0, ancho*0.70, alto))  #RECORTAMOS HORIZONTALMENTE LA IMAGEN AL 70%
 
-# Selecciona la primera página del PDF (puedes iterar sobre todas las páginas si es necesario)
-imagen_0 = jpg[0]
+texto = pytesseract.image_to_string(imagen, lang="spa") # ESTO ES PARA EXTRAER EL TEXTO ("SPA" ES ESPAÑOL)
+lineas = [linea.strip() for linea in texto.split("\n") if linea.strip()] #ORGANIZAMOS EL TEXTO POR LINEAS
 
-#--------------------PARTE 2. PROCESAMIENTO DE LA IMAGEN A TEXTO----------------------------------------------------------------
 
-ancho, alto = imagen_0.size  # Define el tamaño de la imagen
-imagen = imagen_0.crop((0, 0, ancho//2, alto))  # Recorta horizontalmente la imagen al 70%
-
-# Extrae el texto de la imagen
-texto = pytesseract.image_to_string(imagen, lang="spa")
-
-# Organiza el texto por líneas
-lineas = [linea.strip() for linea in texto.split("\n") if linea.strip()]
-
-#--------------------PARTE 3. BUSQUEDA DE LOS DATOS DE INTERES---------------------------------------------------------
+#--------------------PARTE 2. BUSQUEDA DE LOS DATOS DE INTERES---------------------------------------------------------
 
 nombre_completo = "No Encontrado"
 for i, linea in enumerate(lineas):
-    if "NOMBRE" in linea:  # Identifica el texto de las primeras tres líneas después de la palabra NOMBRE
+    if "NOMBRE" in linea: # IDENTIFICA EL TEXTO DE LAS PRIMERAS TRES LINEAS DESPUES DE LA PALABRA NOMBRE
         nombre_completo = " ".join(lineas[i+1:i+4])
         break
         
 domicilio = "No Encontrado"
-for i, linea in enumerate(lineas):  # Identifica el texto de las primeras tres líneas después de la palabra DOMICILIO
+for i, linea in enumerate(lineas):  # IDENTIFICA EL TEXTO DE LAS PRIMERAS TRES LINEAS DESPUES DE LA PALABRA DOMICILIO
     if "DOMICILIO" in linea:
         domicilio = " ".join(lineas[i+1:i+4])
         break
-
-#--------------------PARTE 4. RESULTADOS---------------------------------------------------------------------
+#----------------PARTE 3. RESULTADOS---------------------------------------------------------------------
 
 print("Nombre:", nombre_completo)
 print("Domicilio:", domicilio)
